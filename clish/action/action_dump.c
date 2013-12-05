99311a8000312abf29a5d292d29345df685bc843
Serj Kalichev <serj.kalichev@gmail.com>
2013-12-05 16:24:19 +0400

    Fix dump functions

--- clish/action/action_dump.c
+++ clish/action/action_dump.c
@@ -11,12 +11,9 @@ void clish_action_dump(const clish_action_t *this)
 	lub_dump_printf("action(%p)\n", this);
 	lub_dump_indent();
 
-	lub_dump_printf("script  : %s\n",
-		this->script ? this->script : "(null)");
-	lub_dump_printf("builtin : %s\n",
-		this->builtin ? this->builtin : "(null)");
-	lub_dump_printf("shebang : %s\n",
-		this->shebang ? this->shebang : "(null)");
+	lub_dump_printf("script  : %s\n", LUB_DUMP_STR(this->script));
+	lub_dump_printf("builtin : %s\n", LUB_DUMP_STR(this->builtin));
+	lub_dump_printf("shebang : %s\n", LUB_DUMP_STR(this->shebang));
 
 	lub_dump_undent();
 }
